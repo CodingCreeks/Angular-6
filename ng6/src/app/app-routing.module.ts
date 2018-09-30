032702a6b2b95611d11543fe277674e3644f01bf
CodingCreeks <halappa.manjunatha@gmail.com>
2018-09-30 08:37:46 -0400

    Initial Commit

---
+++ ng6/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { DetailsComponent } from './details/details.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'posts', component: PostsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
