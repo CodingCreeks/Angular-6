ef732be6f0182ab594db135027833b75d55bf8ea
CodingCreeks <halappa.manjunatha@gmail.com>
2018-10-17 22:40:28 -0400

    created the reactive form and basic templete

--- ng6-Demo/src/app/app.module.ts
+++ ng6-Demo/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
 import { ListEmployeesComponent } from './employee/list-employees/list-employees.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, CreateEmployeeComponent, ListEmployeesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
